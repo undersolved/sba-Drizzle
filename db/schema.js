94fe0939774327c7dcae0cdf39469de8219477a4
Undersolved <singh.bhupendra.h@gmail.com>
2026-08-07 12:08:48 +0530

    docker v-update

--- db/schema.js
+++ db/schema.js
@@ -4,5 +4,6 @@ export const usersTable = pgTable("users", {
 	id: uuid().primaryKey().defaultRandom(),
 	name: varchar({ length: 255 }).notNull(),
 	email: varchar({ length: 255 }).notNull().unique(),
-    password: text().notNull()
+	password: text().notNull(),
+	salt: text().notNull(),
 });
