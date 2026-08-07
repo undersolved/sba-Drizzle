19efb6961309a5136b07562eb35177d802811585
Undersolved <singh.bhupendra.h@gmail.com>
2026-08-07 11:39:53 +0530

    schema

---
+++ db/schema.js
@@ -0,0 +1,8 @@
+import { uuid, pgTable, varchar, text } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+	id: uuid().primaryKey().defaultRandom(),
+	name: varchar({ length: 255 }).notNull(),
+	email: varchar({ length: 255 }).notNull().unique(),
+    password: text().notNull()
+});
